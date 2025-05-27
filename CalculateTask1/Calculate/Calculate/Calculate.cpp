b9248ec217b46098f7a417bb213ae4b06b2835b0
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-05-27 17:34:07 +0900

    DELETE: 주석삭제

--- CalculateTask1/Calculate/Calculate/Calculate.cpp
+++ CalculateTask1/Calculate/Calculate/Calculate.cpp
@@ -57,19 +57,18 @@ array<int, 5> insertionSort(const array<int, 5>& numbers, int choiceSortNumber)
  
 	if (choiceSortNumber == 1) // 오름차순
 	{
-		// number = 94, 32, 77, 56, 89
 		for (int i = 1; i < sortNumberList.size(); ++i)
 		{
-			int key = sortNumberList[i]; // 32
-			int j = i - 1; //0, -1
+			int key = sortNumberList[i];
+			int j = i - 1;
 
 			// key보다 큰 원소들을 오른쪽으로 한 칸씩 이동
-			while (j >= 0 && sortNumberList[j] > key) // 94 > 32
+			while (j >= 0 && sortNumberList[j] > key)
 			{
-				sortNumberList[j + 1] = sortNumberList[j]; // sortNumberList[1] = 94 -> [94, 94]
+				sortNumberList[j + 1] = sortNumberList[j];
 				--j;
 			}
-			sortNumberList[j + 1] = key;
+			sortNumberList[j + 1] = key; // 배열 첫번쨰 요소에 sortNumberList[i] 할당
 		}
 	}
 	else if (choiceSortNumber == 2) // 내림차순
